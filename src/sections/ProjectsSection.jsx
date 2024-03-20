b7b22819378a5eba7cdc2e8e41cb3c0964b03a00
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-03-20 19:08:39 -0300

    add featured projects data

--- src/sections/ProjectsSection.jsx
+++ src/sections/ProjectsSection.jsx
@@ -1,22 +1,20 @@
 import React from "react";
+import featured from "../projects/featured.js";
 
 function ProjectsSection() {
   return(
     <section className="bg-night min-h-screen text-center p-16">
         <h2 className="font-gentium italic text-5xl py-3">My projects</h2>
-        <div className="flex flex-col md:flex-row justify-center">
-          <div className="w-full md:w-1/3 p-3">
-            <h3 className="font-chivo text-3xl text-argBlue py-3">Project 1</h3>
-            <p className="font-chivo py-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
-          </div>
-          <div className="w-full md:w-1/3 p-3">
-            <h3 className="font-chivo text-3xl text-argBlue py-3">Project 2</h3>
-            <p className="font-chivo py-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
-          </div>
-          <div className="w-full md:w-1/3 p-3">
-            <h3 className="font-chivo text-3xl text-argBlue py-3">Project 3</h3>
-            <p className="font-chivo py-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
-          </div>
+        <div className="flex flex-col justify-center">
+          {featured.map((project) => (
+            <div key={project.id} className="w-full md:w-full p-3 flex">
+              <img className="w-3/5" src={project.image} alt={project.title} />
+              <div className="">
+                <h3 className="font-chivo text-3xl text-argBlue py-3">{project.title}</h3>
+                <p className="font-chivo py-3 bg-richBlack">{project.description}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
   );
