2ce5990139bf1e63ba0aa814994d6f12f8e35e08
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-03-23 18:41:21 -0300

    fix featured project image

--- src/components/Projects/FeaturedProject.jsx
+++ src/components/Projects/FeaturedProject.jsx
@@ -5,23 +5,23 @@ function FeaturedProject({ project, alignRight }) {
   return (
     <div
       key={project.id}
-      className={`w-full min-h-96 md:w-full relative md:pb-20 flex ${
-        alignRight ? 'flex-row-reverse' : ''
+      className={`w-full md:w-full relative md:pb-20 min-h-80 flex my-2 md:my-4 lg:my-8 ${
+        alignRight ? 'md:justify-end' : ''
       }`}
     >
 
-      <div className={`w-full md:w-7/12 overflow-hidden bg-prussiaBlue absolute ${alignRight ? 'left-0' : 'right-0'}`}>
+      <div className={`h-full md:h-auto md:w-7/12 overflow-hidden md:overflow-visible bg-prussiaBlue flex justify-center absolute ${alignRight ? 'left-0' : 'right-0'}`}>
         <motion.img
           whileHover={{ scale: 1.03, opacity: 1 }}
           whileTap={{ scale: 0.95 }}
           transition={{ duration: 0.5 }}
-          className="w-full opacity-20 md:opacity-70"
+          className="h-full min-w-max md:min-w-0 md:h-auto opacity-15 md:opacity-70"
           src={project.image}
           alt={project.title}
         />
       </div>
 
-      <div className={`${alignRight ? 'md:text-right' : 'md:text-left'} md:w-1/2 z-10 flex flex-col`}>
+      <div className={`${alignRight ? 'md:text-right ' : 'md:text-left'} w-full md:w-1/2 p-4 md:p-0 z-10 flex flex-col justify-between`}>
         <h4 className="text-2xl hover:text-argBlue p-2 md:px-0">
           <a
             href={project.link}
@@ -34,7 +34,7 @@ function FeaturedProject({ project, alignRight }) {
 
         <p className="p-2 md:bg-prussiaBlue opacity-90">{project.description}</p>
 
-        <ul className={`p-2 md:px-0 flex gap-2 w-full ${alignRight ? 'md:justify-end' : 'md:justify-start'}`}>
+        <ul className={`p-2 md:px-0 flex gap-2 w-full flex-wrap ${alignRight ? 'md:justify-end' : 'md:justify-start'}`}>
           {project.technologies.map((tech) => (
             <li key={tech} className="font-chivo text-argBlue py-2">
               {tech}
--- src/sections/ContactSection.jsx
+++ src/sections/ContactSection.jsx
@@ -2,12 +2,12 @@ import ContactForm from '../components/ContactForm';
 
 function ContactSection() {
   return (
-    <section className="bg-richBlack min-h-screen flex flex-col md:flex-row-reverse md:px-36 md:py-28">
-      <ContactForm />
+    <section className="bg-richBlack min-h-screen flex flex-col md:flex-row md:px-36 md:py-28">
       <div className="px-3 w-full md:w-1/2">
         <h2 className="text-5xl pb-3">Contact me</h2>
         <p className="py-3 opacity-90">I&apos;m always interested in hearing about new projects, so if you&apos;d like to chat please get in touch.</p>
       </div>
+      <ContactForm />
     </section>
   );
 }
