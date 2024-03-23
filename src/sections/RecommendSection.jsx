1262fb6ed4c9b855c149d3dce2d6de659a0b4bf5
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-03-23 18:43:20 -0300

    fix linter errors

--- src/sections/RecommendSection.jsx
+++ src/sections/RecommendSection.jsx
@@ -1,9 +1,7 @@
-const Recommend = () => {
-  return (
-    <section className="bg-night min-h-screen flex justify-center text-center">
-      <h2 className="font-medium text-3xl md:text-5xl">Recommendations</h2>
-    </section>
-  );
-}
+const Recommend = () => (
+  <section className="bg-night min-h-screen flex justify-center text-center">
+    <h2 className="font-medium text-3xl md:text-5xl">Recommendations</h2>
+  </section>
+);
 
 export default Recommend;
--- src/sections/SkillsSection.jsx
+++ src/sections/SkillsSection.jsx
@@ -1,9 +1,7 @@
-const SkillsSection = () => {
-  return(
-    <section className="bg-night min-h-screen flex justify-center text-center">
-      <h2 className="font-medium text-5xl">Skills</h2>
-    </section>
-  )
-}
+const SkillsSection = () => (
+  <section className="bg-night min-h-screen flex justify-center text-center">
+    <h2 className="font-medium text-5xl">Skills</h2>
+  </section>
+);
 
 export default SkillsSection;
