bbd71e2fb48aa6ee83bec292fb3fec5f094112ae
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-03-25 19:21:06 -0300

    Create first working version of the tilt cards

--- src/components/Card/TiltCard.jsx
+++ src/components/Card/TiltCard.jsx
@@ -1,91 +1,30 @@
-import React, { useRef } from "react";
-import {
-  motion,
-  useMotionTemplate,
-  useMotionValue,
-  useSpring,
-} from "framer-motion";
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
 
-const Example = () => {
-  return (
-    <div className="">
-      <TiltCard />
-    </div>
-  );
-};
+const Card = ({ title, icon }) => {
+  const tiltRef = useRef();
+  const [tilt, setTilt] = useState({});
 
-const ROTATION_RANGE = 32.5;
-const HALF_ROTATION_RANGE = 32.5 / 2;
-
-const TiltCard = () => {
-  const ref = useRef(null);
-
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
-
-  const xSpring = useSpring(x);
-  const ySpring = useSpring(y);
-
-  const transform = useMotionTemplate`rotateX(${xSpring}deg) rotateY(${ySpring}deg)`;
-
-  const handleMouseMove = (e) => {
-    if (!ref.current) return [0, 0];
-
-    const rect = ref.current.getBoundingClientRect();
-
-    const width = rect.width;
-    const height = rect.height;
-
-    const mouseX = (e.clientX - rect.left) * ROTATION_RANGE;
-    const mouseY = (e.clientY - rect.top) * ROTATION_RANGE;
-
-    const rX = (mouseY / height - HALF_ROTATION_RANGE) * -1;
-    const rY = mouseX / width - HALF_ROTATION_RANGE;
-
-    x.set(rX);
-    y.set(rY);
-  };
-
-  const handleMouseLeave = () => {
-    x.set(0);
-    y.set(0);
+  const calculateTilt = (e) => {
+    const { left, top, width, height } = tiltRef.current.getBoundingClientRect();
+    const x = (e.clientX - left) / width;
+    const y = (e.clientY - top) / height;
+    const rotation = 60 * (x - 0.5);
+    const tilt = { x: `${10 * (0.5 - y)}deg`, y: `${rotation}deg` };
+    setTilt(tilt);
   };
 
   return (
     <motion.div
-      ref={ref}
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseLeave}
-      style={{
-        transformStyle: "preserve-3d",
-        transform,
-      }}
-      className="relative h-96 w-72 rounded-xl bg-gradient-to-br from-indigo-300 to-violet-300"
+      className="relative w-10 h-10 md:w-28 md:h-28 bg-prussiaBlue rounded-lg shadow-lg overflow-hidden flex"
+      ref={tiltRef}
+      onMouseMove={calculateTilt}
+      onMouseLeave={() => setTilt({})}
+      style={{ rotateX: tilt.x, rotateY: tilt.y }}
     >
-      <div
-        style={{
-          transform: "translateZ(75px)",
-          transformStyle: "preserve-3d",
-        }}
-        className="absolute inset-4 grid place-content-center rounded-xl bg-white shadow-lg"
-      >
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/threedsmax/threedsmax-original.svg" alt="Bootstrap logo"
-          style={{
-            transform: "translateZ(75px)",
-          }}
-          className="mx-auto text-4xl"
-        />
-        <p
-          style={{
-            transform: "translateZ(50px)",
-          }}
-          className="text-center text-2xl font-bold"
-        >
-          HOVER ME
-        </p>
-      </div>
+      <img className="w-full h-full object-cover m-auto" src={icon} alt={title} />
     </motion.div>
   );
 };
 
-export default Example;
+export default Card;
--- src/sections/SkillsSection.jsx
+++ src/sections/SkillsSection.jsx
@@ -1,9 +1,16 @@
-import Example from "../components/Card/TiltCard";
+import Card from "../components/Card/TiltCard";
+import stack from "../data/techs";
 
 const SkillsSection = () => (
   <section className="bg-night min-h-screen flex flex-col justify-center text-center px-10 py-20 md:px-16 lg:px-32">
     <h2 className="font-medium text-5xl">Skills</h2>
-    <Example />
+    {stack.length && (
+      <div className="grid grid-cols-2 md:grid-cols-6 gap-8 mt-10">
+        {stack.map((tech, index) => (
+          <Card key={index} title={tech.title} icon={tech.icon} />
+        ))}
+      </div>
+    )}
   </section>
 );
 
