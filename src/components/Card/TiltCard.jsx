833596a5fe72abaa53c0ba996f3d8e1d0ada0d42
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-03-26 20:10:26 -0300

    add dinamic background to hamburger button

--- src/components/Card/TiltCard.jsx
+++ src/components/Card/TiltCard.jsx
@@ -21,10 +21,10 @@ const Card = ({ title, icon }) => {
     dragElastic={0.1}
     whileHover={{ scale: 1.1 }}
     className="flex flex-col items-center w-full h-full">
-      <Tilt options={defaultOptions} className="relative w-14 h-14 md:w-20 md:h-20 rounded-lg justify-center shadow-lg overflow-hidden flex bg-gradient-to-br from-richBlack via-argBlue to-magnolia">
+      <Tilt options={defaultOptions} className="relative w-14 h-14 md:w-20 md:h-20 rounded-lg justify-center shadow-lg overflow-hidden p-2 flex bg-gradient-to-br from-richBlack via-argBlue to-magnolia">
         <img className="w-full h-full object-cover m-auto" src={icon} alt={title} loading="lazy" />
       </Tilt>
-      <h3 className="text-center text-lg md:text-2xl font-chivo text-magnolia opacity-80 py-3">{title}</h3>
+      <h3 className="text-center text-lg md:text-xl font-chivo text-magnolia opacity-80 py-3">{title}</h3>
     </motion.div>
     
   );
--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -52,20 +52,24 @@ const Navbar = () => {
           </ul>
         </div>
       </motion.nav>
-      <div className="fixed z-30 md:hidden">
-        <button className="relative group">
-          <div className="relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-magnolia ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
+      <div className="fixed z-30 md:hidden p-2">
+        <button aria-label='mobile menu' className="relative group">
+          <motion.div 
+          initial={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+          animate={{ backgroundColor: navBgColor }}
+          transition={{ duration: 0.5 }}
+          className="relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-argBlue ring-opacity-30 duration-200 shadow-md">
             <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
-              <div className="bg-richBlack h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-y-6 delay-100"></div>
-              <div className="bg-richBlack h-[2px] w-7 rounded transform transition-all duration-300 group-focus:translate-y-6 delay-75"></div>
-              <div className="bg-richBlack h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-y-6"></div>
+              <div className="bg-magnolia h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-y-6 delay-100"></div>
+              <div className="bg-magnolia h-[2px] w-7 rounded transform transition-all duration-300 group-focus:translate-y-6 delay-75"></div>
+              <div className="bg-magnolia h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-y-6"></div>
 
               <div className="absolute items-center justify-between transform transition-all duration-500 top-2.5 -translate-x-10 group-focus:translate-x-0 flex w-0 group-focus:w-12">
-                <div className="absolute bg-richBlack h-[2px] w-5 transform transition-all duration-500 rotate-0 delay-300 group-focus:rotate-45"></div>
-                <div className="absolute bg-richBlack h-[2px] w-5 transform transition-all duration-500 -rotate-0 delay-300 group-focus:-rotate-45"></div>
+                <div className="absolute bg-magnolia h-[2px] w-5 transform transition-all duration-500 rotate-0 delay-300 group-focus:rotate-45"></div>
+                <div className="absolute bg-magnolia h-[2px] w-5 transform transition-all duration-500 -rotate-0 delay-300 group-focus:-rotate-45"></div>
               </div>
             </div>
-          </div>
+          </motion.div>
         </button>
       </div>
     </>
