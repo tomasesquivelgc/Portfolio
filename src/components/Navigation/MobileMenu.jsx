9d253e8234d47c1df2e2e10646992240798bfd28
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-03-28 14:06:56 -0300

    add animation to button

--- src/components/Navigation/MobileMenu.jsx
+++ src/components/Navigation/MobileMenu.jsx
@@ -22,20 +22,28 @@ const MobileMenu = ({ navBgColor }) => {
           animate={{ backgroundColor: navBgColor }}
           transition={{ duration: 0.5 }}
           className="relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-argBlue ring-opacity-30 duration-200 shadow-md">
-            <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
               {isOpen ? (
-                <div className="absolute items-center justify-between transform top-2.5 -translate-x-10 group-focus:translate-x-0 flex w-0 group-focus:w-12">
+                <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                className="absolute items-center justify-between transform top-2.5 -translate-x-10 group-focus:translate-x-0 flex w-0 group-focus:w-12">
                   <div className="absolute bg-magnolia h-[2px] w-5 transform rotate-45"></div>
                   <div className="absolute bg-magnolia h-[2px] w-5 transform -rotate-45"></div>
+                </motion.div>
                 </div>
               ) : (
-                <>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
                   <div className="bg-magnolia h-[2px] w-7 "></div>
                   <div className="bg-magnolia h-[2px] w-7 "></div>
                   <div className="bg-magnolia h-[2px] w-7 "></div>
-                </>
+                </motion.div>
               )}
-            </div>
           </motion.div>
         </button>
         {isOpen && <MobileMenuContent handleClose={handleBackdropClose} />}
