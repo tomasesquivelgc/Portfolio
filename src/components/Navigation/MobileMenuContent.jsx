3abd09741155ac1e5ddfc667ea65dcec8400e637
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-03-28 13:54:29 -0300

    delete transition classes from hamburger

--- src/components/Navigation/MobileMenuContent.jsx
+++ src/components/Navigation/MobileMenuContent.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const MobileMenuContent = ({handleClose}) => {
   return (
     <Backdrop onClick={handleClose} justify={"justify-start"}>
-        <motion.div
+        <motion.nav
           onClick={(e) => e.stopPropagation()}
           className="bg-richBlack  opacity-100 p-4 rounded-lg fixed lg:max-w-1/2 w-11/12 h-1/2 m-auto flex flex-col align-center z-50"
           initial="hidden"
@@ -19,7 +19,7 @@ const MobileMenuContent = ({handleClose}) => {
             <li>Recommendations</li>
             <li>Contact</li>
           </ul>
-        </motion.div>
+        </motion.nav>
       </Backdrop>
   )
 }
