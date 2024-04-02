1f48c1e5a2c7afff11a6dbb176fcc323f927cdae
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-04-02 16:41:02 -0300

    style modal info

--- src/components/Modal/ModalInfo.jsx
+++ src/components/Modal/ModalInfo.jsx
@@ -1,16 +1,19 @@
-const ModalInfo = ({info}) =>{
-    return (
-      <div className="flex">
-        {info.map((info) => (
-          <div className="w-1/3" key={info.title}>
-            <img className="" src={info.image} alt={info.title} />
-              <h2>{info.title}</h2>
-              <p>{info.description}</p>
+const ModalInfo = ({info}) => {
+  return (
+    <div className="flex gap-4 justify-between w-full max-h-72">
+      {info.map((info) => (
+        <div className="w-1/3 relative border border-magnolia bg-prussiaBlue" key={info.title}>
+          <div className="w-full h-full flex items-center justify-center overflow-hidden">
+            <img className="max-h-full max-w-none" src={info.image} alt={info.title} />
           </div>
-        ))}
-      </div>
-      
-    )
+          <div className="absolute bottom-0 w-full bg-night opacity-80 p-4">
+            <h2>{info.title}</h2>
+            <p>{info.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default ModalInfo;
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -15,14 +15,14 @@ const Modal = ({ handleClose, info }) => {
     <Backdrop onClick={handleClose} justify="justify-center">
       <motion.div
         onClick={(e) => e.stopPropagation()}
-        className="bg-richBlack opacity-100 p-4 rounded-lg fixed lg:max-w-1/2 w-11/12 h-3/4 m-auto flex flex-col align-center z-50"
+        className="bg-richBlack opacity-100 p-4 rounded-lg fixed lg:max-w-1/2 w-11/12 h-3/4 m-auto flex flex-col align-center justify-between z-50"
         initial="hidden"
         animate="visible"
         exit="exit"
       >
         <ModalInfo info={info} />
         <motion.button
-          whileHover={{ scale: 1.1 }}
+          whileHover={{ scale: 1.03 }}
           className="bg-argBlue py-2 px-4 rounded-lg hover:bg-argBlueDark"
           onClick={handleClose}
         >
