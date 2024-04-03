f55c66f66b44334e4fd780216a9276f064009d7e
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-04-03 15:18:58 -0300

    give text hierarchy

--- src/components/Modal/ModalInfo.jsx
+++ src/components/Modal/ModalInfo.jsx
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 const ModalInfo = ({ info }) => (
   <>
     <div className="text-center py-2">
-      <h3 className="text-3xl pb-2">{info.title}</h3>
+      <h3 className="text-4xl pb-2">{info.title}</h3>
       <p className="">{info.text}</p>
     </div>
     <div className="flex flex-col md:flex-row gap-4 justify-around w-full max-h-96 py-8">
       {info.data.map((data) => (
-        <div className="md:w-1/4 w-full relative border border-magnolia bg-prussiaBlue h-2/5 md:h-auto flex md:flex-col" key={info.title}>
+        <div className="md:w-1/4 w-full relative bg-prussiaBlue h-2/5 md:h-auto flex md:flex-col" key={info.title}>
           <div className="w-full h-full flex md:items-center md:justify-center overflow-hidden">
             <img className="max-h-full min-h-full max-w-none" src={data.image} alt={info.title} />
           </div>
-          <div className="absolute bottom-0 w-full bg-night opacity-75 h-full md:h-1/2 p-4 max-h-full">
-            <h2 className="">{data.title}</h2>
+          <div className="absolute bottom-0 right-0 md:left-0 w-1/2 md:w-full bg-night opacity-80 h-full md:h-1/2 p-4 max-h-full">
+            <h4 className="text-lg md:text-base lg:text-lg text-argBlue">{data.title}</h4>
             <p>{data.description}</p>
           </div>
         </div>
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -15,19 +15,22 @@ const Modal = ({ handleClose, info }) => {
     <Backdrop onClick={handleClose} justify="justify-center">
       <motion.div
         onClick={(e) => e.stopPropagation()}
-        className="bg-richBlack opacity-100 p-4 rounded-lg fixed lg:max-w-1/2 w-11/12 h-full md:h-5/6 m-auto flex flex-col align-center justify-between z-50"
+        className="bg-richBlack p-4 opacity-100 rounded-lg fixed lg:max-w-1/2 w-11/12 h-full md:h-5/6 m-auto flex flex-col align-center justify-between z-50"
         initial="hidden"
         animate="visible"
         exit="exit"
       >
         <ModalInfo info={info} />
-        <motion.button
-          whileHover={{ scale: 1.03 }}
-          className="bg-argBlue py-2 px-4 rounded-lg hover:bg-argBlueDark"
-          onClick={handleClose}
-        >
-          Close
-        </motion.button>
+          <div className='flex justify-center'>
+            <motion.button
+              whileHover={{ scale: 1.03 }}
+              className="bg-argBlue px-4 py-2 rounded-lg hover:bg-argBlueDark w-1/2 align-middle"
+              onClick={handleClose}
+            >
+              Close
+            </motion.button> 
+          </div>
+            
       </motion.div>
     </Backdrop>
   );
