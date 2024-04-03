3d636a38855fb1cd9b53f58b23e2405e10c53bc0
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-04-03 15:47:32 -0300

    add expandable text and scrollable on mobile

--- src/components/Modal/ModalInfo.jsx
+++ src/components/Modal/ModalInfo.jsx
@@ -1,28 +1,49 @@
 import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
-const ModalInfo = ({ info }) => (
-  <>
-    <div className="text-center py-2">
-      <h3 className="text-4xl pb-2">{info.title}</h3>
-      <p className="">{info.text}</p>
+const ModalInfo = ({ info }) => {
+  const [expandedItem, setExpandedItem] = useState(null);
+
+  return (
+    <div className="overflow-scroll md:overflow-visible">
+      <div className="text-center py-2">
+        <h3 className="text-4xl pb-2">{info.title}</h3>
+        <p className="">{info.text}</p>
+      </div>
+      <div className="flex flex-col md:flex-row gap-4 justify-around w-full h-full md:max-h-96 py-8">
+        {info.data.map((data, index) => (
+          <motion.div
+            className={`md:w-1/3 w-full border-2 border-argBlue relative bg-prussiaBlue h-1/3 md:h-auto flex md:flex-col ${
+              expandedItem === index ? 'md:h-auto' : ''
+            }`}
+            key={index}
+            onClick={() => setExpandedItem(index)}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className="w-full h-full flex md:items-center md:justify-center overflow-hidden">
+              <img className="max-h-full min-h-full max-w-none" src={data.image} alt={info.title} />
+            </div>
+            <motion.div
+              className={`absolute bottom-0 right-0 md:left-0 h-full md:h-1/2 w-1/2 md:w-full bg-night opacity-80 p-4 ${
+                expandedItem === index ? 'md:h-full w-full' : ''
+              }`}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              <h4 className="text-lg md:text-base lg:text-lg">{data.title}</h4>
+              <p className="text-argBlue">{data.description}</p>
+              {expandedItem === index && <p>{data.expanded}</p>}
+            </motion.div>
+          </motion.div>
+        ))}
+      </div>
     </div>
-    <div className="flex flex-col md:flex-row gap-4 justify-around w-full max-h-96 py-8">
-      {info.data.map((data) => (
-        <div className="md:w-1/4 w-full relative bg-prussiaBlue h-2/5 md:h-auto flex md:flex-col" key={info.title}>
-          <div className="w-full h-full flex md:items-center md:justify-center overflow-hidden">
-            <img className="max-h-full min-h-full max-w-none" src={data.image} alt={info.title} />
-          </div>
-          <div className="absolute bottom-0 right-0 md:left-0 w-1/2 md:w-full bg-night opacity-80 h-full md:h-1/2 p-4 max-h-full">
-            <h4 className="text-lg md:text-base lg:text-lg text-argBlue">{data.title}</h4>
-            <p>{data.description}</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  </>
-
-);
-
+  );
+};
 ModalInfo.propTypes = {
   info: PropTypes.shape({
     title: PropTypes.string.isRequired,
