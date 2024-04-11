e1618d5c47435f89290752af562d8e456e947f6c
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-04-11 16:22:17 -0300

    reorganize tech stack

--- src/data/techs.js
+++ src/data/techs.js
@@ -1,107 +1,107 @@
 const stack = [
   {
-    title: 'Ruby',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ruby/ruby-original.svg',
+    title: 'JavaScript',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg',
     index: 1,
   },
   {
-    title: 'Rails',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rails/rails-plain.svg',
+    title: 'Ruby',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ruby/ruby-original.svg',
     index: 2,
-  },
-  {
-    title: 'PostgreSQL',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg',
-    index: 3,
   },
   {
     title: 'React',
     icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg',
+    index: 3,
+  },
+  {
+    title: 'Rails',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rails/rails-plain.svg',
     index: 4,
   },
   {
-    title: 'Redux',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg',
+    title: 'Node.js',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg',
     index: 5,
-  },
-  {
-    title: 'Javascript',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg',
-    index: 6,
   },
   {
     title: 'HTML',
     icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg',
-    index: 7,
+    index: 6,
   },
   {
     title: 'CSS3',
     icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg',
-    index: 8,
+    index: 7,
   },
   {
-    title: 'TailwindCSS',
+    title: 'Tailwind CSS',
     icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg',
-    index: 9,
+    index: 8,
   },
   {
     title: 'Bootstrap',
     icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg',
+    index: 9,
+  },
+  {
+    title: 'React Router',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/reactrouter/reactrouter-original.svg',
     index: 10,
   },
   {
-    title: 'D3',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/d3js/d3js-original.svg',
+    title: 'Redux',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg',
     index: 11,
   },
   {
-    title: 'ReactRouter',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/reactrouter/reactrouter-original.svg',
+    title: 'D3.js',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/d3js/d3js-original.svg',
     index: 12,
-  },
-  {
-    title: 'Vitejs',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg',
-    index: 13,
-  },
-  {
-    title: 'WebPack',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/webpack/webpack-original.svg',
-    index: 14,
-  },
-  {
-    title: 'Bash',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bash/bash-original.svg',
-    index: 15,
-  },
-  {
-    title: 'Eslint',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/eslint/eslint-original.svg',
-    index: 16,
   },
   {
     title: 'Framer Motion',
     icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/framermotion/framermotion-original.svg',
-    index: 17,
+    index: 13,
+  },
+  {
+    title: 'PostgreSQL',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg',
+    index: 14,
   },
   {
     title: 'Git',
     icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg',
-    index: 18,
+    index: 15,
   },
   {
     title: 'GitHub',
     icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg',
+    index: 16,
+  },
+  {
+    title: 'Bash',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bash/bash-original.svg',
+    index: 17,
+  },
+  {
+    title: 'Webpack',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/webpack/webpack-original.svg',
+    index: 18,
+  },
+  {
+    title: 'Vite.js',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg',
     index: 19,
+  },
+  {
+    title: 'ESLint',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/eslint/eslint-original.svg',
+    index: 20,
   },
   {
     title: 'Jest',
     icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-plain.svg',
-    index: 20,
-  },
-  {
-    title: 'Nodejs',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg',
     index: 21,
   },
 ];
