33103abf882f31d2e178a1a5e1a66cfeedbde013
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-04-16 20:56:54 -0300

    de-structure techlist into own componen

--- src/sections/SkillsSection.jsx
+++ src/sections/SkillsSection.jsx
@@ -1,7 +1,6 @@
 import { useRef } from 'react';
 import { useInView } from 'framer-motion';
-import Card from '../components/Card/TiltCard';
-import stack from '../data/techs';
+import TechList from '../components/Card/TechList';
 
 const SkillsSection = () => {
   const ref = useRef(null);
@@ -9,19 +8,7 @@ const SkillsSection = () => {
   return (
     <section ref={ref} id="skills" className="bg-night min-h-screen flex flex-col justify-center text-center px-10 py-20 md:px-16 lg:px-32">
       <h2 className="font-medium text-5xl">Skills</h2>
-      {stack.length && (
-      <div className="grid grid-cols-3 gap-4 md:grid-cols-5 lg:grid-cols-6 md:gap-10 mt-10 m-auto">
-        {stack.map((tech) => (
-          <Card
-            isInView={isInView}
-            key={tech.title}
-            title={tech.title}
-            icon={tech.icon}
-            index={tech.index}
-          />
-        ))}
-      </div>
-      )}
+      <TechList isInView={isInView} />
     </section>
   );
 };
