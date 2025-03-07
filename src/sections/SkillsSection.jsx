1611c54d7cbe62a11514290d2ca26561677c4d24
Tomas Esquivel <tomasesquivel@yahoo.com>
2025-03-07 16:14:09 -0300

    attach ref to card title

--- src/sections/SkillsSection.jsx
+++ src/sections/SkillsSection.jsx
@@ -1,16 +1,10 @@
-import { useRef } from 'react';
-import { useInView } from 'framer-motion';
 import TechList from '../components/Card/TechList';
 
-const SkillsSection = () => {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
-  return (
-    <section ref={ref} id="skills" className="bg-night min-h-screen flex flex-col justify-center text-center px-10 py-20 md:px-16 lg:px-32">
-      <h2 className="font-medium text-5xl">Skills</h2>
-      <TechList isInView={isInView} />
-    </section>
-  );
-};
+const SkillsSection = () => (
+  <section id="skills" className="bg-night min-h-screen flex flex-col justify-center text-center px-10 py-20 md:px-16 lg:px-32">
+    <h2 className="font-medium text-5xl">Skills</h2>
+    <TechList />
+  </section>
+);
 
 export default SkillsSection;
