56348dcb3c33ba81f5b6306dd88ccc729e7cee18
Tomas Esquivel <tomasesquivel@yahoo.com>
2025-03-08 22:41:57 -0300

    change focus outline

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -4,11 +4,34 @@ function ContactForm() {
   return (
     <form action="https://formspree.io/f/xwkjrkqw" method="post" className="flex flex-col w-full md:w-1/2 bg-prussiaBlue p-8 rounded-xl gap-2">
 
-      <input className="w-full bg-magnolia text-black p-2 rounded-2xl" id="full-name" type="text" name="name" placeholder="Your name" maxLength="30" required />
+      <input 
+        className="w-full bg-magnolia text-black p-2 rounded-2xl focus:outline-none focus:ring-2 focus:ring-argBlue" 
+        id="full-name" 
+        type="text" 
+        name="name" 
+        placeholder="Your name" 
+        maxLength="30" 
+        required 
+      />
 
-      <input className="w-full bg-magnolia text-black p-2 rounded-2xl" id="email" type="email" name="email" placeholder="Your Email address" required />
+      <input 
+        className="w-full bg-magnolia text-black p-2 rounded-2xl focus:outline-none focus:ring-2 focus:ring-argBlue" 
+        id="email" 
+        type="email" 
+        name="email" 
+        placeholder="Your Email address" 
+        required 
+      />
 
-      <textarea className="py-1 bg-magnolia text-black p-2 rounded-2xl" id="msg" name="user_message" maxLength="500" placeholder="Write me something..." rows="5" required />
+      <textarea 
+        className="py-1 bg-magnolia text-black p-2 rounded-2xl focus:outline-none focus:ring-2 focus:ring-argBlue" 
+        id="msg" 
+        name="user_message" 
+        maxLength="500" 
+        placeholder="Write me something..." 
+        rows="5" 
+        required 
+      />
 
       <motion.input
         whileHover={{ scale: 1.05 }}
