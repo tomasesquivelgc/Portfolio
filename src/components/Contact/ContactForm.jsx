8a31f99bf791a460f9ffedbd1390a1b6eccb7f47
Tomas Esquivel <tomasesquivel@yahoo.com>
2025-03-08 22:38:01 -0300

    add style to the form

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -2,22 +2,17 @@ import { motion } from 'framer-motion';
 
 function ContactForm() {
   return (
-    <form action="https://formspree.io/f/xwkjrkqw" method="post" className="flex flex-col w-full md:w-1/2">
-      <label htmlFor="full-name" className="pb-1">
-        Full name:
-        <input className="w-full bg-magnolia text-night" id="full-name" type="text" name="name" placeholder="Full name" maxLength="30" required />
-      </label>
+    <form action="https://formspree.io/f/xwkjrkqw" method="post" className="flex flex-col w-full md:w-1/2 bg-prussiaBlue p-8 rounded-xl gap-2">
 
-      <label htmlFor="email" className="py-1">
-        Email:
-        <input className="w-full bg-magnolia text-night" id="email" type="email" name="email" placeholder="Email address" required />
-      </label>
+      <input className="w-full bg-magnolia text-black p-2 rounded-2xl" id="full-name" type="text" name="name" placeholder="Your name" maxLength="30" required />
 
-      <textarea className="py-1 bg-magnolia text-night" id="msg" name="user_message" maxLength="500" placeholder="Write me something..." required />
+      <input className="w-full bg-magnolia text-black p-2 rounded-2xl" id="email" type="email" name="email" placeholder="Your Email address" required />
+
+      <textarea className="py-1 bg-magnolia text-black p-2 rounded-2xl" id="msg" name="user_message" maxLength="500" placeholder="Write me something..." rows="5" required />
 
       <motion.input
         whileHover={{ scale: 1.05 }}
-        className="p-2 w-full rounded-lg bg-argBlue hover:cursor-pointer my-2 text-night"
+        className="p-2 w-full rounded-2xl bg-gradient-to-r from-argBlue to-argBlue/70 hover:cursor-pointer my-2 text-black font-bold"
         type="submit"
         value="Submit"
       />
